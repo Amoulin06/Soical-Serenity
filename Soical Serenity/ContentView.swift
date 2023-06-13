1db754b6656fd1ffb2e2167af9a2c4c89dd67265
Abbie <abbigail.moulin@gmail.com>
2023-06-13 09:30:58 -0500

    Initial commit

--- Soical Serenity/ContentView.swift
+++ Soical Serenity/ContentView.swift
@@ -10,7 +10,7 @@ import CoreData
 
 struct ContentView: View {
     @Environment(\.managedObjectContext) private var viewContext
-
+//this is  a change
     @FetchRequest(
         sortDescriptors: [NSSortDescriptor(keyPath: \Item.timestamp, ascending: true)],
         animation: .default)
